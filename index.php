b79cdf6a807862d131d87d69f2c7f223890ec846
Nicole Han <35822487+nicoella@users.noreply.github.com>
2022-12-31 03:29:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-//ini_set('display_errors', '0');
-//ini_set('log_errors', 1);
-error_reporting(^E_ERROR | ^E_WARNING | ^E_PARSE);
+ini_set('display_errors', '0');
+ini_set('log_errors', 1);
 ?>
 
 <!DOCTYPE html>
